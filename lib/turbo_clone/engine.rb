fd06cc5df69a6a3142f4480ea0f54a15bbfb2974
Angel Diaz <adiaz004@yahoo.com>
2024-06-27 22:22:48 -0700

    implement turbo_frame_tag

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -1,5 +1,12 @@
+
 module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
+
+    initializer 'turbo_clone.action_controller' do |app|
+      ActiveSupport.on_load :action_controller_base do
+        helper TurboClone::FramesHelper
+      end
+    end
   end
 end
