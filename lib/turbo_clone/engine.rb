226d0f8f2366ec3d18274cde6259e6e9d6cff862
Angel Diaz <adiaz004@yahoo.com>
2024-06-30 16:53:39 -0700

    add  dnew turbostream for destroy and create

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -3,6 +3,10 @@ module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
 
+    initializer "turbo.media_type" do
+      Mime::Type.register "text/vnd.turbo-stream.html", :turbo_stream
+    end
+
     initializer 'turbo_clone.action_controller' do |app|
       ActiveSupport.on_load :action_controller_base do
         helper TurboClone::FramesHelper
